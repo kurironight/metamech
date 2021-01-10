e4c23cefc727fff9e4509faa3b5eb2d80a25645e
kurironight <titechcycsin17@gmail.com>
2021-01-10 12:50:05 +0900

    連結部分の修正

--- env/gym_fem.py
+++ env/gym_fem.py
@@ -32,19 +32,21 @@ class FEMGym(MetamechGym):
         X_DOF = np.arange(2*(ny+1)-1, 2*(nx+1)*(ny+1), 2*(ny+1))
         self.FIXDOF = np.concatenate([X_DOF, Y_DOF])
         self.displace_DOF = 2*nx*(ny+1)+2  # 強制変位を起こす場所
-
+        """
+        力を付加する場合の条件
         F = np.zeros(2 * (nx + 1) * (ny + 1), dtype=np.float64)
         F[self.displace_DOF-1] = -1
         self.F = F
+        """
 
         disp = np.zeros((2 * (nx + 1) * (ny + 1)), dtype=np.float64)
         disp[self.displace_DOF-1] = -1
         self.disp = disp
 
         # 構造が繋がっているかを確認する時，確認するメッシュ位置のindex
-        self.check_output_mesh_index = (0, 0)
-        self.check_input_mesh_index = (0, nx-1)
-        self.check_freeze_mesh_index = (ny-1, int(nx/2))
+        self.check_output_mesh_index = (ny-1, 0)
+        self.check_input_mesh_index = (ny-1, nx-1)
+        self.check_freeze_mesh_index = (0, int(nx/2))
 
     def extract_rho_for_fem(self):
         nodes_pos, edges_indices, edges_thickness = self.extract_node_edge_info()
@@ -59,13 +61,15 @@ class FEMGym(MetamechGym):
 
     def calculate_simulation(self):
         rho = self.extract_rho_for_fem()
-        #U = FEM(rho, self.FIXDOF, self.F)
-
-        #displacement = np.array([U[0], U[1]])
-        #output_vectors = np.array([1, 0])
-        #efficiency = np.dot(output_vectors, displacement)
-        #
-        #print("力：\n", efficiency)
+        """
+        力を付加する場合の計算方法
+        U = FEM(rho, self.FIXDOF, self.F)
+        displacement = np.array([U[0], U[1]])
+        output_vectors = np.array([1, 0])
+        efficiency = np.dot(output_vectors, displacement)
+        
+        print("力：\n", efficiency)
+        """
         U = FEM_displacement(rho, self.FIXDOF, np.zeros(
             (2 * (self.pixel + 1) * (self.pixel + 1)), dtype=np.float64), self.disp)
 
@@ -74,20 +78,15 @@ class FEMGym(MetamechGym):
         output_vectors = np.array([1, 0])
         efficiency = np.dot(output_vectors, displacement)
 
-        print("変位版：\n", efficiency)
-
         return efficiency
 
     def confirm_graph_is_connected(self):
         # グラフが全て接続しているか確認
 
         rho = self.extract_rho_for_fem().astype(np.uint8)
-
         _, markers = cv2.connectedComponents(rho)
         if markers[self.check_output_mesh_index] == markers[self.check_input_mesh_index] & \
                 markers[self.check_output_mesh_index] == markers[self.check_freeze_mesh_index]:
-
-            self.render("image_connected.png")
             return True
         else:
             return False
